e0e7e481f9716dafa3f91e7e71cba90591a3ff94
Francisco <francisco.juan.dev@gmail.com>
2026-02-10 22:50:32 +0100

    Profundizando un poco en el tema de hoy

--- Python/Dia2/variables.py
+++ Python/Dia2/variables.py
@@ -11,6 +11,12 @@ nombre_completo = nombre + " " + apellido
 edad = 42
 ahorros = 550
 jubilacion = 67 - edad
+a = 500
+b = 500
+frase = "Python es genial"
+puntuación = 0.085743
+variable_inicial = " hola mundo "
+variable_final = variable_inicial.strip()
 
 # Cálculo automático
 coste_total = horas_estimadas * precio_hora
@@ -24,3 +30,19 @@ print(f"Nuevas horas (como texto): {horas_estimadas}")
 print(resumen)
 print(f"Usuario registrado: {nombre_completo}")
 print(f"Te faltan {jubilacion} años para jubilarte y tienes {ahorros} euros")
+print(f"valor de a: {a} - ID en memoria: {id(a)}")
+print(f"Valor de b: {b} - ID en memoria: {id(b)}")
+print(f"¿Son el mismo objeto?: {a is b}")
+a = a + 1
+print("Sumemos 1 en a")
+print(f"valor de a: {a} - ID en memoria: {id(a)}")
+print(f"Valor de b: {b} - ID en memoria: {id(b)}")
+print(f"¿Son el mismo objeto?: {a is b}")
+print(frase.upper())
+print(frase.title())
+print(frase.replace("genial", "increíble"))
+print(frase)
+print(f"Tu puntuación es {puntuación:.2%}")
+print(f"La frase inicial es: {variable_inicial}")
+print(f"La frase limpia es: {variable_final}")
+print(f"La frase limpia tiene {len(variable_final)} caracteres")
