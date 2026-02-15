fa7bffe68b63095f53479d79ea42a9d3aa7f35ef
Francisco <francisco.juan.dev@gmail.com>
2026-02-15 09:20:28 +0100

    Últimas correccioes a  calculadora.py

--- Python/Dia6/Calculadora.py
+++ Python/Dia6/Calculadora.py
@@ -1,12 +1,45 @@
 def sumar(a: float, b: float) -> float:
+    """Suma dos números reales
+    
+    Args:
+        a (float): Primer sumando
+        b (float): Segundo sumando
+    
+    Returns:
+        Float: devuelve la suma de ambos números."""
     return a + b
 def restar(a: float, b:float) -> float:
+    """Resta dos números reales
+    
+    Args:
+        a (float): El inuendo
+        b (float): El sustraendo
+    
+    Returns:
+        Float: devuelve la resta de ambos números."""
     return a - b
 def multiplicar(a: float, b: float) -> float:
+    """Multiplica dos números reales
+    
+    Args:
+        a (float): Primer factor
+        b (float): Segundo factor
+    
+    Returns:
+        Float: devuelve la multiplicación de ambos números."""
     return a * b
 def dividir(a: float, b: float) -> float:
-    if b <= 0:
-        return 0
+    """Divide dos números reales
+    
+    Args:
+        a (float): El dividendo
+        b (float): El divisor
+    
+    Returns:
+        Float: El cociente o None si el divisor es cero
+    """
+    if b == 0:
+        return None
     else:
         return a / b
 
@@ -34,14 +67,15 @@ def main():
         if opcion == "3":
             a = float(input("Dame el primer factor: "))
             b = float(input("Dame el segundo factor: "))
+            c = multiplicar(a,b)
             print(f"El resultado es: {c}")
         
         if opcion == "4":
             a = float(input("Dame el dividendo: "))
             b = float(input("Dame el divisor: "))
             c = dividir(a, b)
-            if c == 0:
-                print("El divisor debe ser mayor a 0") 
+            if c == None:
+                print("Error: El divisor deve ser diferente de 0") 
             else:
                 print(f"El resultado es: {c}")
 
